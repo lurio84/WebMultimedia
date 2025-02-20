eefe6ba43353e72231fcf5bad8df655f857f7fbf
lurio84 <lucas.ronquillo@gmail.com>
2025-02-20 02:42:35 +0100

    Login interface

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,28 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import UserPage from "./pages/UserPage";
+import { useState } from "react";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <div>
-      <nav>
-        <Link to="/">Inicio</Link> | <Link to="/user/1">Ver Usuario 1</Link>
-      </nav>
+      {!isAuthenticated ? (
+        <AuthPage onAuthSuccess={() => setIsAuthenticated(true)} />
+      ) : (
+        <>
+          <nav>
+            <Link to="/">Inicio</Link> | <Link to="/user/1">Ver Usuario 1</Link>
+          </nav>
 
-      <Routes>
-        <Route path="/" element={<h1>Bienvenido</h1>} />
-        <Route path="/user/:id" element={<UserPage />} />
-      </Routes>
+          <Routes>
+            <Route path="/" element={<h1>Bienvenido</h1>} />
+            <Route path="/user/:id" element={<UserPage />} />
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
---
+++ Frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./AuthPage.css";
+
+function AuthPage({ onAuthSuccess }) {
+  const [isRegister, setIsRegister] = useState(false);
+
+  return (
+    <div className="auth-container">
+      <div className="auth-box">
+        <h2>{isRegister ? "Register" : "Login"}</h2>
+        <form>
+          {isRegister && <input type="text" placeholder="Name" required />}
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button type="submit">{isRegister ? "Rergister" : "Login"}</button>
+        </form>
+        <p onClick={() => setIsRegister(!isRegister)}>
+          {isRegister
+            ? "Already have an account? Login"
+            : "Don't have an account? Register"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AuthPage;
