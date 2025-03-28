dafaf64399e548da583df26f0797efdb29fbc720
lurio84 <lucas.ronquillo@gmail.com>
2025-03-28 14:54:39 +0100

    Added button to return to user page

--- Frontend/src/pages/EditNotePage.jsx
+++ Frontend/src/pages/EditNotePage.jsx
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getNoteById, updateNoteById } from "../api/noteService";
+import { useNavigate } from "react-router-dom";
 import "./EditNotePage.css";
 
 function EditNotePage() {
   const { noteId } = useParams();
   const [note, setNote] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchNote = async () => {
@@ -35,7 +37,17 @@ function EditNotePage() {
         onChange={handleChange}
       />
       <textarea name="content" value={note.content} onChange={handleChange} />
-      <button onClick={handleSave}>Guardar</button>
+      <div className="edit-note-buttons">
+        <button
+          className="action-button"
+          onClick={() => navigate(`/user/${note.userId}`)}
+        >
+          ← Volver
+        </button>
+        <button className="action-button" onClick={handleSave}>
+          Guardar
+        </button>
+      </div>
     </div>
   );
 }
